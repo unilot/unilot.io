dd11a9b7d0fa4fa4bab33c872f269f08fb1ec7c6
Alexander Lushnikov <alexander.aka.alegz@gmail.com>
2017-09-22 02:02:12 +0500

    Added fields to game model to record number of players and prize amount. Added migration to update database structure. Added alias method to request actual active games. Improved work with contract, added event handling to update contract id once it created and event is fired. Updated libraries and dependencies.

--- backend/serializers/game.py
+++ backend/serializers/game.py
@@ -1,8 +1,27 @@
+from decimal import Decimal
 from rest_framework import serializers
-from backend.models import Game
+from backend.models import Game, ExchangeRate
 
 
 class PublicGameSerializer(serializers.ModelSerializer):
+    prize_amount_fiat = serializers.SerializerMethodField(method_name='convert_prize_amount_to_fiat')
+
+    def convert_prize_amount_to_fiat(self, obj):
+        """
+        :param obj:
+        :type obj: Game
+        :return: float
+        """
+
+        latest_exchange_rate = ExchangeRate.objects.order_by('-created_at').first()
+        """
+        :var latest_exchange_rate:
+        :type latest_exchange_rate: ExchangeRate
+        """
+
+        return float(Decimal(obj.prize_amount) * Decimal(latest_exchange_rate.rate))
+
+
     class Meta:
         model = Game
-        fields = ('id', 'status', 'type', 'smart_contract_id', 'started_at', 'ending_at')
+        fields = ('id', 'status', 'type', 'smart_contract_id', 'prize_amount', 'prize_amount_fiat', 'num_players', 'started_at', 'ending_at')
