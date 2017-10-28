283146577cdf90a6042fbdd83d50da8bb3249d23
Alexander Lushnikov <alexander.aka.alegz@gmail.com>
2017-10-28 15:18:02 +0500

    Added endpoint for games history. Added method to game model to select history.

--- backend/urls.py
+++ backend/urls.py
@@ -4,7 +4,8 @@ from sportloto.settings import DEBUG
 
 urlpatterns = [
     url(r'^games(|/)$', games.GamesView.as_view(), name='games'),
-    url(r'^games/(?P<pk>\d+)/winners(|/)$', games.GamePrizesView.as_view(), name='games'),
+    url(r'^games/archived(|/)$', games.GameArchivedView.as_view(), name='games_archived'),
+    url(r'^games/(?P<pk>\d+)/winners(|/)$', games.GamePrizesView.as_view(), name='game_winners'),
     url(r'^device(|/)$', devices.DeviceCreateView.as_view(), name='device')
 ]
 
