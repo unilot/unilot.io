fb316b56a5ee66f9a982b7353179dc493ec1d26c
Alexander Lushnikov <alexander.aka.alegz@gmail.com>
2017-11-16 02:10:25 +0500

    Added model DeviceSettings. Added migration for model. Added model to admin processing list. Added endpoint to accept device settings with os and token. Added serializer to process request for device and settings. Updated djangorestframework package. Refactored processing push messages sending for IOS devices. Added logic that considers IOS device settings.

--- backend/urls.py
+++ backend/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     url(r'^games/(?P<pk>\d+)(|/)$', games.SingleGameView.as_view(), name='game'),
     url(r'^games/archived(|/)$', games.GameArchivedView.as_view(), name='games_archived'),
     url(r'^games/(?P<pk>\d+)/winners(|/)$', games.GamePrizesView.as_view(), name='game_winners'),
-    url(r'^device(|/)$', devices.DeviceCreateView.as_view(), name='device')
+    url(r'^device(|/)$', devices.DeviceCreateView.as_view(), name='device_create'),
+    url(r'^device/settings(|/)$', devices.DeviceCreateWithSettingsView.as_view(), name='device_create_with_settings')
 ]
 
 if DEBUG:
--- backend/utils/push.py
+++ backend/utils/push.py
@@ -1,4 +1,5 @@
 from push_notifications.models import GCMDevice, APNSDevice
+from sportloto import settings
 
 
 class PushHelper:
@@ -10,15 +11,26 @@ class PushHelper:
 
     @staticmethod
     def inform_apns_devices(push_message, token=None):
-        if token is None:
-            devices = APNSDevice.objects.filter()
-        elif isinstance(token, (list, tuple)):
-            devices = APNSDevice.objects.filter(registration_id__in=token)
-        else:
-            devices = APNSDevice.objects.get(registration_id=token)
+        prefix = 'settings_apns_device'
+        filter_data = {'%s__%s' % (prefix, field_name):value
+                       for field_name, value in push_message.get_required_settings().items()}
 
-        devices.send_message(message=push_message.data.get('message', {}).get('en'), extra=push_message.data,
-                             content_available=True, mutable_content=True, sound='chime.aiff')
+        for language_tuple in settings.LANGUAGES:
+            language_code = language_tuple[0]
+            filter_data['%s__language' % (prefix)] =  language_code
+
+            try:
+                if token is None:
+                    devices = APNSDevice.objects.filter(**filter_data)
+                elif isinstance(token, (list, tuple)):
+                    devices = APNSDevice.objects.filter(registration_id__in=token, **filter_data)
+                else:
+                    devices = [APNSDevice.objects.get(registration_id=token, **filter_data)]
+
+                devices.send_message(message=push_message.data.get('message', {}).get(language_code), extra=push_message.data,
+                                     content_available=True, mutable_content=True, sound='chime.aiff')
+            except APNSDevice.DoesNotExist:
+                pass
 
     @staticmethod
     def inform_gcm_devices(push_message, token=None):
