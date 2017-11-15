a9a4d11fc40fa3f02809971384f1dc806c991da3
Alexander Lushnikov <alexander.aka.alegz@gmail.com>
2017-11-16 03:09:22 +0500

    Optimized device query. Added support of apns devices without settings.

--- backend/utils/push.py
+++ backend/utils/push.py
@@ -1,3 +1,4 @@
+from django.db.models.query_utils import Q
 from push_notifications.models import GCMDevice, APNSDevice
 from sportloto import settings
 
@@ -14,23 +15,25 @@ class PushHelper:
         prefix = 'settings_apns_device'
         filter_data = {'%s__%s' % (prefix, field_name):value
                        for field_name, value in push_message.get_required_settings().items()}
+        legacy_filter_data = {prefix:None}
 
         for language_tuple in settings.LANGUAGES:
             language_code = language_tuple[0]
             filter_data['%s__language' % (prefix)] =  language_code
 
-            try:
-                if token is None:
-                    devices = APNSDevice.objects.filter(**filter_data)
-                elif isinstance(token, (list, tuple)):
-                    devices = APNSDevice.objects.filter(registration_id__in=token, **filter_data)
-                else:
-                    devices = [APNSDevice.objects.get(registration_id=token, **filter_data)]
+            if token is not None:
+                tokens = list(token) if isinstance(token, (list, tuple)) else [token]
+                filter_data['registration_id__in'] = tokens
+                legacy_filter_data['registration_id__in'] = tokens
 
-                devices.send_message(message=push_message.data.get('message', {}).get(language_code), extra=push_message.data,
-                                     content_available=True, mutable_content=True, sound='chime.aiff')
-            except APNSDevice.DoesNotExist:
-                pass
+            if language_code == 'en':
+                devices = APNSDevice.objects.filter(Q(**filter_data) | Q(**legacy_filter_data))
+            else:
+                devices = APNSDevice.objects.filter(Q(**filter_data))
+
+            devices.send_message(message=push_message.data.get('message', {}).get(language_code),
+                                 extra=push_message.data, content_available=True, mutable_content=True,
+                                 sound='chime.aiff')
 
     @staticmethod
     def inform_gcm_devices(push_message, token=None):
